64b52c730a3b73c14119f0dba8c817470119702c
Alex Pantelides <alex.pantelides@gmail.com>
2016-11-27 17:04:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# symphony-commerce-coding-challenge
+A simple store
+
+## Instructions
+
+  1. Download it!
+  2. execute npm install
+  3. execute npm start
+  4. navigate to localhost:3000
+  5. Enjoy!
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import { Jumbotron, Table, Well, Form, FormGroup, FormControl, ControlLabel, Button, ListGroup, ListGroupItem, Glyphicon, Col }  from 'react-bootstrap';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  componentWillMount () {
+  }
+
+  render() {
+    return (
+      <h1>Hello world!</h1>
+    );
+  }
+}
